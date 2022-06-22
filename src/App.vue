5e40c635f714b51ae1108d543a52378a5b2bc4f7
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-22 15:11:35 -0400

    drawer map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="map"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+var center = [-3.10719, -60.0261]; //centro do Mapa
+// var map = L.map('map').setView(center, 6);
+var editableLayers = new L.FeatureGroup();
+var drawPluginOptions = {
+  position: "topright",
+  draw: {
+    polygon: {
+      allowIntersection: false, // Restricts shapes to simple polygons
+      drawError: {
+        color: "#e1e100", // Color the shape will turn when intersects
+        message: "<strong>Oh snap!<strong> you can't draw that!", // Message that will show when intersect
+      },
+      shapeOptions: {
+        color: "#97009c",
+      },
+    },
+    // disable toolbar item by setting it to false
+    polyline: false,
+    circle: false, // Turns off this drawing tool
+    rectangle: false,
+    marker: false,
+  },
+  edit: {
+    featureGroup: editableLayers, //REQUIRED!!
+    remove: false,
+  },
+};
+var drawControl = new L.Control.Draw(drawPluginOptions);
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      map: undefined,
+    };
+  },
+  methods: {
+    initMap() {
+      this.map = L.map("map").setView(center, 0); //Criando Mapa
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: 'Data © <a href="http://osm.org/copyright">OpenStreetMap</a>',
+        maxZoom: 18,
+      }).addTo(this.map);
+      L.marker(center).addTo(this.map);
+      this.map.addLayer(editableLayers);
+      this.map.addControl(drawControl);
+      this.map.on("draw:created", function (e) {
+        var type = e.layerType,
+          layer = e.layer;
+        if (type === "marker") {
+          layer.bindPopup("POPUP");
+        }
+        editableLayers.addLayer(layer);
+      });
+    },
+  },
+  mounted() {
+    this.initMap();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  height: 100%;
+}
+#map {
+  height: 100%;
 }
 </style>
